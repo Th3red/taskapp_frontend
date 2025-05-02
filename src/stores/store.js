06ab70744a0ff3d30e70addf895eb80af78f9d53
Th3red <hiroschmitz@gmail.com>
2025-05-02 16:35:15 -0600

    update stores

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk';
 import authReducer from "../reducers/authReducer";
-import movieReducer from "../reducers/movieReducer";
+
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'development') {
 const store = configureStore({
     reducer: {
         auth: authReducer,
-        movie: movieReducer
+       
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(thunk)
